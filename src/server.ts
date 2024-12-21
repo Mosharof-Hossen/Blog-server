4b58947a834620afd267bc853024540e9aeec01f
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-12-21 12:40:32 +0600

    INTERNAL_SERVER_ERROR fixed

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import config from "./config";
 import app from "./app";
+import { Server } from "http";
 
+let server: Server
 
-
-async function server() {
+async function main() {
     try {
         await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
+        server = app.listen(config.port, () => {
             console.log(`Server is running on ${config.port}`);
         })
     } catch (err) {
@@ -15,4 +16,20 @@ async function server() {
     }
 }
 
-server();
+main();
+
+process.on("unhandledRejection", () => {
+    console.log(`😈 unhandledRejection is detected , shutting down ...`);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+})
+
+process.on("uncaughtException", () => {
+    console.log(`😈 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
+})
+
+// a = 25
